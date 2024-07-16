45c6195071fda41d4dcb97dd0b383d9ae384d612
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-16 19:10:31 -0400

    feat: added network data state classes

--- lib/core/resources/data_state.dart
+++ lib/core/resources/data_state.dart
@@ -1,8 +1,25 @@
 import 'package:dio/dio.dart';
 
+/*
+Network state class
+ */
+
 abstract class DataState<T>{
   final T ? data;
   final DioError ? error;
+  final T ? loading;
 
-  const DataState({this.data}, {this.error});
+  const DataState({this.data, this.error, this.loading});
+}
+
+class DataSuccess<T> extends DataState<T> {
+  const DataSuccess(T data) : super(data: data);
+}
+
+class DataFailed<T> extends DataState<T> {
+  const DataFailed(DioError error) : super(error: error);
+}
+
+class DataLoading<T> extends DataState<T> {
+  const DataLoading(T loading) : super(loading: loading);
 }
