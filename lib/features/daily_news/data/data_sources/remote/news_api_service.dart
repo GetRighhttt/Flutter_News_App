c3f223d5a2d60639c416709c84316b8251aa043f
Stefan Bayne <stefanbayne@220022522s-MacBook-Pro.local>
2024-07-16 20:22:15 -0400

    feat: starter retrofit code.

---
+++ lib/features/daily_news/data/data_sources/remote/news_api_service.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+import 'package:dio/dio.dart';
+import 'package:news_app/core/constants/constants.dart';
+import 'package:retrofit/http.dart';
+import '../../models/article.dart';
+part 'news_api_service.g.dart';
+
+@RestApi(baseUrl: newsAPIBaseUrl)
+abstract class NewsApiService {
+  factory NewsApiService(Dio dio) = _NewsApiService;
+
+  @GET('/top-headlines')
+  Future<HttpResponse<List<ArticleModel>>> getNewsArticles({
+    @Query("apiKey") String ? apiKey,
+    @Query("country") String ? country,
+    @Query("category") String ? category
+    });
+}
